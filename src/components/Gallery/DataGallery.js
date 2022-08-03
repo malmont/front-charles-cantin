06662feeaea761767106cc3a31a0c011e20c9fa0
Almont michel <michel.almont@gmail.com>
2022-08-03 19:15:11 -0400

    ajout photo galery

---
+++ src/components/Gallery/DataGallery.js
@@ -0,0 +1,91 @@
+import { v4 as uuidv4 } from "uuid";
+
+const DataGallery = [
+  {
+    id: uuidv4(),
+    lien: "/imgs/1.jpeg",
+    categorie: "Famille"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/2.jpeg",
+    categorie: "Grossesse"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/3.jpeg",
+    categorie: "Bébé"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/4.jpeg",
+    categorie: "Bébé"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/5.jpeg",
+    categorie: "Mariage"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/6.jpeg",
+    categorie: "Couple"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/7.jpeg",
+    categorie: "Famille"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/8.jpeg",
+    categorie: "Couple"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/9.jpeg",
+    categorie: "Bébé"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/10.jpeg",
+    categorie: "Bébé"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/11.jpeg",
+    categorie: "Mariage"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/12.jpeg",
+    categorie: "Mariage"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/13.jpeg",
+    categorie: "Famille"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/14.jpeg",
+    categorie: "Grossesse"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/15.jpeg",
+    categorie: "Mariage"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/16.jpeg",
+    categorie: "Couple"
+  },
+  {
+    id: uuidv4(),
+    lien: "/imgs/17.jpeg",
+    categorie: "Grossesse"
+  },
+];
+
+export default DataGallery;
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import React from 'react';
 import {useTrail,animated} from 'react-spring';
-import {v4 as uuidv4} from 'uuid'
+import {v4 as uuidv4} from 'uuid';
+import DataGallery from './DataGallery';
+
 
 export default function Gallery() {
 
   
-  const trail = useTrail(18,{
+  const trail = useTrail(DataGallery.length,{
     from:{
      opacity:0,
      x:20
@@ -20,18 +22,27 @@ export default function Gallery() {
   return (
     <Wrapper>
    <h1>Gallerie</h1>
-<WrapperList>
+      <WrapperList>
 
-{trail.map((cardStyle,index)=>{
-        return <animated.div
-        key={uuidv4()}
-        style={cardStyle}
-        >
-           <WrapperCard/>
-        </animated.div>
+      {trail.map((cardStyle,index)=>{
+              return <animated.div
+              key={uuidv4()}
+              style={cardStyle}
+              >
+                <WrapperCard>
 
-      })}
-  </WrapperList>
+                <animated.div >  
+                <WrapDIvPhoto >
+                  <img  className="img-fluid" src={DataGallery[index].lien} alt="" />      
+                  </WrapDIvPhoto>
+                </animated.div>
+
+                </WrapperCard>
+
+              </animated.div>
+
+            })}
+        </WrapperList>
     </Wrapper>
   )
 }
@@ -54,14 +65,25 @@ display: flex;
 `;
 
 const WrapperCard = styled.div `
- width: 200px;
-    height: 200px;
+    width: 200px;
+    height: 160px;
     border-radius: 20px;
     display: flex;
     justify-content: center;
     align-items: center;
-    background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
+    background: #70ACAC;
     margin: 15px;
     opacity: 1;
 
+`;
+const WrapDIvPhoto = styled.div`
+
+padding:20px;
+width:230px;
+margin : 0 auto;
+border-radius:15px;
+
+& img{
+  border-radius:15px;
+}
 `;
