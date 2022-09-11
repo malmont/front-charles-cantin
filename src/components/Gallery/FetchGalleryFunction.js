fa66915db7b0110540dfa38e0c0cd005c066fc49
Almont michel <michel.almont@gmail.com>
2022-09-11 18:05:37 -0400

    finalisation factorisation

---
+++ src/components/Gallery/FetchGalleryFunction.js
@@ -0,0 +1,56 @@
+import HomeAPI from "../../services/HomeAPI";
+import {v4 as uuidv4} from 'uuid';
+
+   
+    const dataGallery=[];
+    const URLImage="https://31.220.53.103";
+  async function arrayLoadingFunction(paramGallery) {
+
+    if (paramGallery==="Bébé"){
+  
+      const data = await HomeAPI.GalleryAPI();
+      return data.data[0].attributes.Baby.data;
+    }
+    if (paramGallery==="Famille"){
+      const data = await HomeAPI.GalleryAPI();
+      return data.data[0].attributes.Famille.data;
+    }
+    if (paramGallery==="Grossesse"){
+      const data = await HomeAPI.GalleryAPI();
+      return data.data[0].attributes.Grossesse.data;
+    }
+    if (paramGallery==="Mariage"){
+      const data = await HomeAPI.GalleryAPI();
+      return data.data[0].attributes.Mariage.data;
+    }
+    if (paramGallery==="Couple"){
+      const data = await HomeAPI.GalleryAPI();
+      return data.data[0].attributes.Couple.data;
+    }
+  
+   
+  }
+  
+    async function fetchGallery(categorie) {
+      
+     const arrayLoading = await arrayLoadingFunction(categorie);
+      arrayLoading.map((item) => {
+  
+         dataGallery.push({
+          id: uuidv4(),
+          lien: URLImage + item.attributes.url,
+          categorie: categorie
+        });
+  
+      });
+      return dataGallery;
+    }
+
+  
+  
+    
+    export default {
+        fetchGallery, 
+
+    }
+
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,137 +2,35 @@ import styled from "styled-components";
 import React, {useState,useEffect} from 'react';
 import {useTrail,animated} from 'react-spring';
 import {v4 as uuidv4} from 'uuid';
-import HomeAPI from "../../services/HomeAPI";
 
+import fetchGalleryFunction from "../Gallery/FetchGalleryFunction"
 
 
 export default function Gallery() {
   const [Gallery,setGallery] = useState([]);
-  const dataGallery=[];
-  // const [isLoading,setIsLoading] = useState(true);
-  // const [posts,setPosts] = useState(null);
-
-  const URLImage="https://31.220.53.103";
+  let dataGallery=[];
+  const [filterGallery,setFilterGallery] = useState(dataGallery);
 
   useEffect(()=>{
+   
     fetchGallery("Bébé");
     fetchGallery("Famille");
     fetchGallery("Grossesse");
     fetchGallery("Mariage");
     fetchGallery("Couple");
-    // fetchGalleryBaby();
-    // fetchGalleryFamily();
-    // fetchGalleryGrossesse();
-    // fetchGalleryMariage();
-    // fetchGalleryCouple();
+
+
   },[]);
 
 
-  async function arrayLoadingFunction(paramGallery) {
-  const data = await HomeAPI.GalleryAPI();
-  if (paramGallery==="Bébé"){
-    return data.data[0].attributes.Baby.data;
-  }
-  if (paramGallery==="Famille"){
-    return data.data[0].attributes.Famille.data;
-  }
-  if (paramGallery==="Grossesse"){
-    return data.data[0].attributes.Baby.data;
-  }
-  if (paramGallery==="Mariage"){
-    return data.data[0].attributes.Mariage.data;
-  }
-  if (paramGallery==="Couple"){
-    return data.data[0].attributes.Couple.data;
-  }
- 
-}
-
   async function fetchGallery(categorie) {
-    
-   const arrayLoading = arrayLoadingFunction(categorie);
-    arrayLoading.map((item) => {
-      dataGallery.push({
-        id: uuidv4(),
-        lien: URLImage + item.attributes.url,
-        categorie: categorie
-      });
-
-    });
+    dataGallery = await fetchGalleryFunction.fetchGallery(categorie);
+    console.log(dataGallery);
+    const newArr = [...Gallery,...dataGallery];
+    setGallery(newArr);
+    setFilterGallery(dataGallery);
   }
 
-  const fetchGalleryBaby = async () =>{
-    const data = await  HomeAPI.GalleryAPI();
-    const arrayLoading = data.data[0].attributes.Baby.data;
-    arrayLoading.map((item)=>{
-       dataGallery.push({
-        id: uuidv4(),
-        lien: URLImage+ item.attributes.url,
-        categorie: "Bébé"
-      })
-     
-    })
-  }
-
-  const fetchGalleryFamily = async () =>{
-    const data = await  HomeAPI.GalleryAPI();
-    const arrayLoading = data.data[0].attributes.Famille.data;
-    arrayLoading.map((item)=>{
-       dataGallery.push({
-        id: uuidv4(),
-        lien: URLImage+ item.attributes.url,
-        categorie: "Famille"
-      })
-     
-    })
-  }
-
-  const fetchGalleryGrossesse = async () =>{
-    const data = await  HomeAPI.GalleryAPI();
-    const arrayLoading = data.data[0].attributes.Grossesse.data;
-    arrayLoading.map((item)=>{
-       dataGallery.push({
-        id: uuidv4(),
-        lien: URLImage+ item.attributes.url,
-        categorie: "Grossesse"
-      })
-     
-    })
-  }
-  const fetchGalleryMariage = async () =>{
-    const data = await  HomeAPI.GalleryAPI();
-    const arrayLoading = data.data[0].attributes.Mariage.data;
-    arrayLoading.map((item)=>{
-       dataGallery.push({
-        id: uuidv4(),
-        lien: URLImage+ item.attributes.url,
-        categorie: "Mariage"
-      })
-     
-    })
-
- 
-
-
-   
-    
-  }
-  const fetchGalleryCouple= async () =>{
-    const data = await  HomeAPI.GalleryAPI();
-    const arrayLoading = data.data[0].attributes.Couple.data;
-    arrayLoading.map((item)=>{
-       dataGallery.push({
-        id: uuidv4(),
-        lien: URLImage+ item.attributes.url,
-        categorie: "Couple"
-      })
-    })
-  const newArr = [...Gallery,...dataGallery];
-  setGallery(newArr);
-  }
-
-  const [filterGallery,setFilterGallery] = useState(dataGallery);
-  
   const trail = useTrail(filterGallery.length,{
 
     from:{
@@ -149,14 +47,14 @@ export default function Gallery() {
 
  const selectGallery= e=>{
   e.preventDefault();
-  console.log(e.target.value)
+  // console.log(e.target.value)
   const newArr = [...Gallery];
-  console.log(Gallery)
+  // console.log(Gallery)
   const newGallery = newArr.filter(item =>{
     if(e.target.value === "All"){return Gallery;}
     return item.categorie === e.target.value;
 })
-console.log(e.target.value)
+// console.log(e.target.value)
 setFilterGallery(newGallery);
 
  }
@@ -165,6 +63,7 @@ setFilterGallery(newGallery);
     <Wrapper>
    <h1>Gallerie</h1>
       <WrapperList>
+        
 <div className="container ">
   <select onChange={selectGallery} class="selectpicker mx-5 mb-5" data-width="100px" aria-label="Default select example">
   <option selected>All</option>
@@ -175,23 +74,24 @@ setFilterGallery(newGallery);
   <option value="Mariage">Mariage</option>
   </select>
   </div>
+  
       {trail.map((cardStyle,index)=>{
-              return <animated.div
-              key={uuidv4()}
-              style={cardStyle}
-              >
+     
+              return <animated.div key={uuidv4()} style={cardStyle}>
+                
                 <WrapperCard>
 
                 <animated.div >  
                 <WrapDIvPhoto >
-                  <img  className="img-fluid" src={process.env.PUBLIC_URL + filterGallery[index].lien} alt="" />      
+                <img  className="img-fluid" src={process.env.PUBLIC_URL + filterGallery[index].lien} alt="" />  
+                  
                   </WrapDIvPhoto>
                 </animated.div>
 
                 </WrapperCard>
 
               </animated.div>
-
+           
             })}
         </WrapperList>
     </Wrapper>
