689f32cc0dbdf18139ea7dfc15b605d2fd1d662f
Almont michel <michel.almont@gmail.com>
2022-09-13 18:07:25 -0400

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
